b98d630fe5359d1e013216569a8eae951655d1c3
vinycxuz <vinyby@gmail.com>
2024-11-25 16:15:54 -0300

    fix

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const dotenv = require('dotenv');
+const fs = require('fs');
+const path = require('path');
 
 dotenv.config();
 
@@ -21,7 +23,16 @@ app.get('/articles', async (req, res) => {
             markdown += `## ${article.title}\n`;
             markdown += `${article.url}\n`;
         });
-        res.send(markdown); // Send the markdown content as response
+
+        const readmePath = path.join(__dirname, 'README.md');
+        const readmeContent = fs.readFileSync(readmePath, 'utf8');
+        const updatedReadme = readmeContent.replace(
+            '<!--START_SECTION:latest-articles-->.*<!--END_SECTION:latest-articles-->',
+            `<!--START_SECTION:latest-articles-->\n${markdown}\n<!--END_SECTION:latest-articles-->`
+        );
+        fs.writeFileSync(readmePath, updatedReadme);
+
+        res.send(markdown); 
     } catch (error) {
         res.status(500).json({ error: 'Erro ao buscar artigos' });
     }
